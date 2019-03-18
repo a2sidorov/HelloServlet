679c0afa42ac2d4e67edb6372d7d820ce33f5c41
justi5ed <a2sidorov@gmail.com>
2019-03-18 15:18:36 +0300

    adding form

--- src/main/java/dev/a2/HelloServlet.java
+++ src/main/java/dev/a2/HelloServlet.java
@@ -10,23 +10,15 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() throws ServletException {
-        // Do required initialization
         message = "Hello World tet";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Set response content type
         response.setContentType("text/html");
-
-        // Actual logic goes here.
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
-        out.println("<h1>test</h1>");
-
-
-
+        request.getRequestDispatcher("/WEB-INF/views/index.jsp")
+                .forward(request, response);
     }
 
     public void destroy() {
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="/test">
+    <input type="text" name="username">
+    <button type="submit">Submit</button>
+
+</form>
+
+
+</body>
+</html>
