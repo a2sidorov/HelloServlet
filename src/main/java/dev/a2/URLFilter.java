c7aa886e504f7eeb4b59c9b99761a91863b34d90
Andrei Sidorov <a2sidorov@gmail.com>
2019-03-18 14:50:39 +0300

    first commit

---
+++ src/main/java/dev/a2/URLFilter.java
@@ -0,0 +1,17 @@
+package dev.a2;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class URLFilter implements Filter {
+
+public void init(FilterConfig filterConfig) throws ServletException {
+        }
+
+public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException {
+
+        }
+
+public void destroy() {
+        }}
